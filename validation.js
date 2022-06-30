ab185188d71df27d8448d8543073ec2786290941
Hugo <huwgoma@gmail.com>
2022-06-30 12:04:30 -0400

    Validation and styling of invalid Name fields

---
+++ validation.js
@@ -0,0 +1,13 @@
+/* Validate Name Fields on Blur */
+
+// Add/Toggle on class="required" if input's value is empty
+function toggleRequiredClass(e){
+    let input = e.target;
+    input.classList.toggle('required', input.value == "");
+}
+
+const fname = document.getElementById('fname');
+fname.addEventListener('blur', toggleRequiredClass);
+
+const lname = document.getElementById('lname');
+lname.addEventListener('blur', toggleRequiredClass);
